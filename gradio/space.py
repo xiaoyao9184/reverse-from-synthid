5fffd59144154801cb1be33b35165411bb127ff5
xiaoyao9184 <6614349+xiaoyao9184@users.noreply.github.com>
2026-08-13 17:08:34 +0800

    feat(space): add space entrypoint

---
+++ gradio/space.py
@@ -0,0 +1,10 @@
+# run gradio in subprocess in reloaded mode
+# huggingface space issue: https://github.com/gradio-app/gradio/issues/10048
+# need disable reload for huggingface space
+import re
+import sys
+from gradio.cli import cli
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw|\.exe)?$', '', sys.argv[0])
+    sys.argv.append(re.sub(r'space\.py$', 'gradio_app.py', sys.argv[0]))
+    sys.exit(cli())
