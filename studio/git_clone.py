e98c0f960b1d6cafa9c61cae86353339d7449b86
xiaoyao9184 <6614349+xiaoyao9184@users.noreply.github.com>
2026-08-15 13:35:56 +0800

    build(studio): git proxy with more print

--- studio/git_clone.py
+++ studio/git_clone.py
@@ -1,14 +1,15 @@
 import os
 import git
 
-REPO_URL = "https://github.com/aloshdenny/reverse-SynthID.git"
+REPO_URL = os.environ.get("GITHUB_PROXY", "") + "https://github.com/aloshdenny/reverse-SynthID.git"
 REPO_BRANCH = 'b11083676fd3ee3ff97ce9d03c0e409e46905902'
-LOCAL_PATH = "./reverse-SynthID"
+LOCAL_PATH = os.path.abspath("./reverse-SynthID")
 
 def install_src():
     if not os.path.exists(LOCAL_PATH):
         print(f"Cloning repository from {REPO_URL}@{REPO_BRANCH} to {LOCAL_PATH}...")
         repo = git.Repo.clone_from(REPO_URL, LOCAL_PATH)
         repo.git.checkout(REPO_BRANCH)
+        print(f"Repository cloned to {LOCAL_PATH}")
     else:
         print(f"Repository already exists at {LOCAL_PATH}")
