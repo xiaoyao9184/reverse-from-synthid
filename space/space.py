2292c2deec7d7911528db27191a06e56725fe634
xiaoyao9184 <6614349+xiaoyao9184@users.noreply.github.com>
2026-08-15 11:01:44 +0800

    feat(space): move to dir

--- space/space.py
+++ space/space.py
@@ -12,5 +12,5 @@ import sys
 from gradio.cli import cli
 if __name__ == '__main__':
     sys.argv[0] = re.sub(r'(-script\.pyw|\.exe)?$', '', sys.argv[0])
-    sys.argv.append(re.sub(r'space\.py$', 'gradio_app.py', sys.argv[0]))
+    sys.argv.append(re.sub(r'space\.py$', 'gradio/gradio_app.py', sys.argv[0]))
     sys.exit(cli())
